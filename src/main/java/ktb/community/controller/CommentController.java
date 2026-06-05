ae72e07608fa973c61cb08455e1c8cf30e782530
wnsgh7368 <sin010820@gmail.com>
2026-06-05 15:17:42 +0900

    refactor: 도메인 책임 분리 : PostController와 PostService에 존재하던 Like 관련 기능을 LikeController와 LikeService로 분리

---
+++ src/main/java/ktb/community/controller/CommentController.java
@@ -0,0 +1,4 @@
+package ktb.community.controller;
+
+public class Comment {
+}
--- src/main/java/ktb/community/controller/PostController.java
+++ src/main/java/ktb/community/controller/PostController.java
@@ -68,26 +68,4 @@ public class PostController {
 
         return ResponseEntity.ok(ApiResponse.success("게시글을 성공적으로 삭제하였습니다."));
     }
-
-    // 게시글 좋아요 등록 API 컨트롤러
-    @PostMapping("/{postId}/likes")
-    public ResponseEntity<ApiResponse<Void>> createLike(
-            @AuthenticationPrincipal Long userId,
-            @PathVariable Long postId) {
-
-        postService.createLike(userId, postId);
-
-        return ResponseEntity.ok(ApiResponse.success("좋아요를 성공적으로 등록하였습니다."));
-    }
-
-    // 게시글 좋아요 삭제 API 컨트롤러
-    @DeleteMapping("/{postId}/likes")
-    public ResponseEntity<ApiResponse<Void>> deleteLike(
-            @AuthenticationPrincipal Long userId,
-            @PathVariable Long postId ) {
-
-        postService.deleteLike(userId, postId);
-
-        return ResponseEntity.ok(ApiResponse.success("좋아요를 성공적으로 취소하였습니다."));
-    }
 }
