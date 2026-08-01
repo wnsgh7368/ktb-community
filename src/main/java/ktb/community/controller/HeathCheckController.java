0b6255d883ea90680cbe2343f9bef3f236a6f18d
wnsgh7368 <sin010820@gmail.com>
2026-08-02 01:38:04 +0900

    feat: ci-cd 완성

--- src/main/java/ktb/community/controller/HeathCheckController.java
+++ src/main/java/ktb/community/controller/HeathCheckController.java
@@ -1,4 +1,4 @@
-package ktb.community.util;
+package ktb.community.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,8 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HeathCheckController {
-    @GetMapping("/aculator/health")
+    @GetMapping("/health")
     public ResponseEntity.BodyBuilder getHealthcheck() {
         return ResponseEntity.ok();
     }
+
+    @GetMapping("/check/rolling")
+    public String checkRolling() { return "hello v1 World!"; }
 }
+
